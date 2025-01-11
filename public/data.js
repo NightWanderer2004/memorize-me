769390d373cfed0c1d96b6a8ac222c0b276839ef
Eduard Povierin <edpovieri@gmail.com>
2025-01-11 20:51:48 +0100

    Add new images. Improve styles

---
+++ public/data.js
@@ -0,0 +1,56 @@
+const data = {
+   years: [
+      {
+         year: 2023,
+         albums: [
+            {
+               month: 'November',
+               photos: ['/images/2.jpg', '/images/1.jpg', '/images/3.jpg'],
+            },
+            {
+               month: 'October',
+               photos: ['/images/5.jpg', '/images/2.jpg', '/images/2.jpg'],
+            },
+            {
+               month: 'September',
+               photos: ['/images/4.jpg', '/images/2.jpg', '/images/3.jpg'],
+            },
+            {
+               month: 'August',
+               photos: ['/images/3.jpg', '/images/1.jpg', '/images/5.jpg'],
+            },
+            {
+               month: 'July',
+               photos: ['/images/5.jpg', '/images/2.jpg', '/images/4.jpg'],
+            },
+         ],
+      },
+      {
+         year: 2022,
+         albums: [
+            {
+               month: 'November',
+               photos: ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg'],
+            },
+            {
+               month: 'October',
+               photos: ['/images/4.jpg', '/images/5.jpg', '/images/6.jpg'],
+            },
+            {
+               month: 'September',
+               photos: ['/images/7.jpg', '/images/8.jpg', '/images/9.jpg'],
+            },
+            {
+               month: 'August',
+               photos: ['/images/10.jpg', '/images/11.jpg', '/images/12.jpg'],
+            },
+            {
+               month: 'July',
+               photos: ['/images/13.jpg', '/images/14.jpg', '/images/15.jpg'],
+            },
+         ],
+      },
+   ],
+}
+
+export default data
--- src/components/Gallery/PhotoGallery.js
+++ src/components/Gallery/PhotoGallery.js
@@ -1,35 +1,16 @@
-import PhotoCard from './PhotoCard'
-
-const photos = [
-   {
-      month: 'November',
-      imageUrl: '/images/sep.jpg',
-   },
-   {
-      month: 'October',
-      imageUrl: '/images/sep.jpg',
-   },
-   {
-      month: 'September',
-      imageUrl: '/images/sep.jpg',
-   },
-   {
-      month: 'August',
-      imageUrl: '/images/sep.jpg',
-   },
-   {
-      month: 'July',
-      imageUrl: '/images/sep.jpg',
-   },
-]
+'use client'
+import data from '../../../public/data'
+import AlbumSet from './AlbumSet'
 
 export default function PhotoGallery() {
    return (
-      <div className='relative px-[88px] overflow-x-scroll flex gap-[120px] mt-[174px] no-scrollbar'>
-         <div className='absolute top-[calc(50%-20px)] -left-[88px] w-full h-[3px] border-line' />
-         {photos.map(photo => (
-            <PhotoCard key={photo.month} month={photo.month} imageUrl={photo.imageUrl} />
-         ))}
+      <div className='relative mt-[150px]'>
+         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-[3px] border-line' />
+         <div className='flex gap-[120px] px-[88px] py-[20px] no-scrollbar overflow-x-auto'>
+            {data.years[0].albums.map(album => (
+               <AlbumSet key={album.month} month={album.month} photos={album.photos} />
+            ))}
+         </div>
       </div>
    )
 }
