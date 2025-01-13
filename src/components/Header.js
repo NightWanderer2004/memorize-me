e5b25cd7dea40dfe01717525a3122484a6592e3d
Eduard Povierin <67825832+NightWanderer2004@users.noreply.github.com>
2025-01-13 13:00:10 +0100

    Revert "add creation history page"

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,13 @@ export default function Header() {
       <header className='container mx-auto px-[16px] lg:px-[88px] flex flex-col lg:flex-row justify-between items-start w-full'>
          <div>
             <Link href='/' className='text-[62px] font-medium tracking-[-2%] leading-[75%] text-primary'>
-               <span className='font-medium'>Memorize</span>
-               <span className='text-zinc-800 font-bold'>Me</span>
+               <span>Memorize</span>
+               <span className='text-accent'> me</span>
             </Link>
             <p className='mt-[18px] text-[21.5px] text-primary tracking-[-1.5%]'>A digital gallery of cherished moments</p>
          </div>
          <nav>
             <ul className='mt-6 lg:mt-0 flex gap-6 text-[18px] text-primary tracking-[-1.5%]'>
-               <li>
-                  <Link href='/' className='hover:text-secondary transition-colors'>
-                     Gallery
-                  </Link>
-               </li>
                <li>
                   <Link href='/creation-history' className='hover:text-secondary transition-colors'>
                      Creation history
