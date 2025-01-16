f0edb614079be493ceb4d602fdd386842bb3ec2e
Eduard Povierin <edpovieri@gmail.com>
2025-01-16 22:03:28 +0100

    Three: fix deploy error

---
+++ src/components/gallery/AlbumSet.js
@@ -0,0 +1,39 @@
+'use client'
+import { motion } from 'framer-motion'
+import PhotoCard from './PhotoCard'
+import { useState } from 'react'
+
+const randomInt = (min, max) => {
+   return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export default function AlbumSet({ month, photos }) {
+   const [rotations, setRotations] = useState(photos.map(() => randomInt(-3.25, 3.25)))
+
+   const handleHover = () => {
+      setRotations(photos.map(() => randomInt(-3.25, 3.25)))
+   }
+
+   return (
+      <div className='relative top-4 [&>div]:last:mr-[45px] xl:[&>div]:last:mr-[120px]'>
+         <motion.div
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.025 }}
+            whileTap={{ scale: 0.99 }}
+            onHoverStart={handleHover}
+            onHoverEnd={handleHover}
+            transition={{
+               ease: 'backOut',
+               duration: 0.55,
+            }}
+            className='relative h-[200px] w-[300px]'
+         >
+            {photos.map((photo, i) => {
+               const rotation = rotations[i]
+               return <PhotoCard key={i} rotation={rotation} imageUrl={photo} />
+            })}
+         </motion.div>
+         <h3 className='mt-3.5 text-xl font-normal text-secondary'>{month}</h3>
+      </div>
+   )
+}
---
+++ src/components/gallery/PhotoCard.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function PhotoCard({ rotation, imageUrl }) {
+   return (
+      <div
+         style={{ transform: `rotate(${rotation}deg)` }}
+         className='cursor-pointer absolute h-[198px] w-[295px] overflow-hidden rounded-[2px] shadow-photo border-2 border-[#F8F9FA] transform transition-transform duration-[400ms] ease-out'
+      >
+         <Image className='pointer-events-none' src={imageUrl} alt={imageUrl} fill />
+      </div>
+   )
+}
