2ad66c0b84e6cbd5f87cc22e29845537a1648433
Eduard Povierin <edpovieri@gmail.com>
2025-01-24 13:00:08 +0100

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-import withPWAInit from "@ducanh2912/next-pwa";
+import withPWAInit from '@ducanh2912/next-pwa'
 
 const withPWA = withPWAInit({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
+   dest: 'public',
+   register: true,
+   skipWaiting: true,
+})
 
 export default withPWA({
+   env: {
+      BASE_URL: process.env.BASE_URL,
+      HASH_SECRET: process.env.HASH_SECRET,
+   },
    images: {
       domains: ['images.unsplash.com'],
    },
-});
+})
